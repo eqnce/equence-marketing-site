1e422f36c9340de16456dc994f17a327ea1be223
Chintan Udani <chintan.udani@monx.co.in>
2026-07-09 16:44:48 +0530

    updated email

--- src/App.jsx
+++ src/App.jsx
@@ -533,7 +533,9 @@ export default function App() {
             <h2>Bring Eqnce to the front of your plant workflow.</h2>
           </div>
           <div className="cta-actions">
-           
+            <a className="button strong" href="bharvi.desai@monx.co.in">
+             bharvi.desai@monx.co.in
+            </a>
             <a className="button subtle top-link" href="#top">
               <span className="top-link-arrow">↑</span>
               <span>Back to top</span>
